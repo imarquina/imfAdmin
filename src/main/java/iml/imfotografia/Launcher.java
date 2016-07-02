66382e942fd0dce48c55c398f022f85477eb07f8
imarquina <imarquina@gmail.com>
2016-07-02 18:06:10 +0200

    Actualizacion

--- src/main/java/iml/imfotografia/Launcher.java
+++ src/main/java/iml/imfotografia/Launcher.java
@@ -1,8 +1,9 @@
 package iml.imfotografia;
 
 import iml.imfotografia.xml.data.XmlConfig;
+import iml.imfotografia.xml.data.XmlTikaParser;
+import org.apache.tika.exception.TikaException;
 import org.xml.sax.SAXException;
-import iml.imfotografia.xml.data.XmlPhotos;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
@@ -23,34 +24,16 @@ public class Launcher {
 
         try {
             XmlConfig config = new XmlConfig("./data/xml/in/config.xml");
-            System.out.println();
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
         /*
-        XmlConfig config = new XmlConfig("title", "infotext", "keywords");
-
-        Galleries cnfGll1 = config.addGalleries();
-        Folder gll1Fld1 = cnfGll1.addFolder();
-        Gallery gll1Gal1 = cnfGll1.addGallery();
-        Folder gll1Fld2 = cnfGll1.addFolder();
-        Folder gll1Fld3 = cnfGll1.addFolder();
-        Multimedia gll1Mul1 = cnfGll1.addMultimedia();
-
-        Folder cnfFld1 = config.addFolder();
-        Gallery fld1Gal1 = cnfFld1.addGallery();
-
-        Section cnfSec1 = config.addSection();
-        Section cnfSec2 = config.addSection();
-        Section cnfSec3 = config.addSection();
-
-        config.elements.put(6, new Title("iñaki marquina"));
-        config.elements.put(6, new Slogan("fotografia"));
-
-        Track cnfTrc1 = config.addTrack();
-
-        config.elements.put(8, new ContactForm("cuenta@dominio.es", "THIS MESSAGE HAS BEEN SENT FROM YOUR WEBSITE: "));
+        try {
+            XmlTikaParser prueba = new XmlTikaParser("./data/xml/in/config.xml");
+        } catch (TikaException e) {
+            e.printStackTrace();
+        }
         */
     }
 }
